e35770785f9ac8681cc27d11da0d412a40fd3dcf
Eden Alemayehu Gebeta <edengebeta210@gmail.com>
2025-07-29 14:16:50 +0300

    Add javascript functions

--- Toast Pop up/toast.js
+++ Toast Pop up/toast.js
@@ -1,1 +1,29 @@
-const 
+const showToastBtn = document.getElementById('show-toast');
+const toast = document.getElementById('toast');
+const closeIcon = document.getElementById('close-icon');
+const acceptButton = document.getElementById('accept-button');
+const closeButton = document.getElementById('close-button');
+
+let toastTimerId; //to store the timer ID
+
+function showToast(){
+    toast.classList.add('show') //this makes the toast visible by applying the class show
+    toastTimerId = setTimeout(()=>{
+        hideToast();
+    },5000);
+}
+
+showToastBtn.addEventListener('click', showToast );
+
+function hideToast(){
+    toast.classList.remove('show');
+    clearTimeout(toastTimerId);
+}
+
+closeIcon.addEventListener('click',hideToast );
+closeButton .addEventListener('click',hideToast );
+
+acceptButton.addEventListener('click', ()=>{
+    alert('Notification Accepted!');
+    hideToast();
+});
